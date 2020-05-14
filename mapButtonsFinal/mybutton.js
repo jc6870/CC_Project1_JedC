e46cb9d68677659c89a1272606565a8f64b0be6d
Jediah <jc6870@nyu.edu>
2020-05-14 13:24:49 -0400

    some code
    radio buttons to switch between different regions

---
+++ mapButtonsFinal/mybutton.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  radio2 = createRadio();
+  radio2.option(' China   ', 1);
+  //radio2.html('<br>');
+  radio2.option(' France   ', 2);
+  radio2.option(' Germany  ', 3);
+  radio2.option(' Japan  ', 4);
+  radio2.option(' USA  ', 5);
+  radio2.changed(mySelectEvent);
+}
+
+function mySelectEvent(){
+  alert(select("r").value())
+}
+function draw() {
+  background(220);
+  console.log(radio2.selected());
+}
